bcc1dba17acf24f3a0163b02a4183bcf64c752e1
tunde Oretade <tundeiness@yahoo.co.uk>
2019-07-27 12:34:09 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-
+require 'pry'
 def fizzbuzz(val) 
   if val % 3 == 0 
     "Fizz"
