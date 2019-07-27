2837e9f9ba65e50814195d8f513c8201fe1ded6e
tunde Oretade <tundeiness@yahoo.co.uk>
2019-07-27 12:28:30 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,10 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz 
-  
+def fizzbuzz(val) 
+  if val % 3 == 0 
+    "Fizz"
+  elsif val % 5 == 0 
+    "Buzz"
+  end 
 end 
